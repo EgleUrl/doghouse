8f8ec51c90045e65fc6db26490cfca12cf8f65bc
EgleUrl <47123888+EgleUrl@users.noreply.github.com>
2024-05-12 13:20:15 +0100

    still fixing paths

--- src/components/homeHeader.jsx
+++ src/components/homeHeader.jsx
@@ -8,7 +8,7 @@ const HomeHeader = () => {
   return (
     <div className='home-header-container'>
       <video loop autoPlay muted>
-        <source src="/doghouse/public/dogHouse.mp4" type="video/mp4" />
+        <source src="./doghouse/public/dogHouse.mp4" type="video/mp4" />
       </video>
       <h1>DOG HOUSE</h1>
       <p>A Place Where Pets <b>Matter</b></p>   
